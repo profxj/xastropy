55c509b70fee014a91896771326c40d965b7a05b
J. Xavier Prochaska <xavier@ucolick.org>
2015-12-08 14:53:23 -0800

    some CGM stuff

--- xastropy/casbah/load_casbah.py
+++ xastropy/casbah/load_casbah.py
@@ -1,23 +1,8 @@
-"""
-#;+ 
-#; NAME:
-#; build_casbah_galaxies
-#;    Version 1.0
-#;
-#; PURPOSE:
-#;    Module for buildling CASBAH galaxy database
-#;   02-Jan-2015 by JXP
-#;-
-#;------------------------------------------------------------------------------
+""" Module for buildling CASBAH galaxy database
 """
 from __future__ import print_function, absolute_import, division, unicode_literals
 
-import numpy as np
-import os, imp, glob, copy
-from astropy.io import fits, ascii
-from astropy import units as u 
-from astropy.coordinates import SkyCoord
-from astropy.table import Table, Column, MaskedColumn
+from astropy.table import Table
 
 #from astropy import constants as const
 from xastropy.casbah import galaxy_data as xcgd
@@ -28,43 +13,44 @@ from xastropy.obs import radec as xra
 
 from xastropy.xutils import xdebug as xdb
 
+
 # SDSS
 def load_field(field):
-	''' Load up CASBAH data for a given field
+    """ Load up CASBAH data for a given field
 
-	Parameters:
-	-----------
-	field: tuple
-	  (Name, ra, dec)
+    Parameters:
+    -----------
+    field: tuple
+      (Name, ra, dec)
 
-	Returns:
-	--------
-	lfield: 	
-	  Loaded IgmGalaxyField class
-	'''
-	reload(xcgmf)
-	reload(xcasbahu)
-	lfield = xcgmf.IgmGalaxyField(field[0], (field[1],field[2]))
+    Returns:
+    --------
+    lfield:
+      Loaded IgmGalaxyField class
+    """
+    reload(xcgmf)
+    reload(xcasbahu)
+    lfield = xcgmf.IgmGalaxyField(field[0], (field[1],field[2]))
 
-	# Load targets
-	targ_file = xcasbahu.get_filename(field,'TARGETS')
-	lfield.targets = Table.read(targ_file,delimiter='|',
-		format='ascii.fixed_width', 
-		fill_values=[('--','0','MASK_NAME')])
+    # Load targets
+    targ_file = xcasbahu.get_filename(field,'TARGETS')
+    lfield.targets = Table.read(targ_file,delimiter='|',
+        format='ascii.fixed_width',
+        fill_values=[('--','0','MASK_NAME')])
 
-	# Load observing details for multi-object follow-up
-	obs_file = xcasbahu.get_filename(field,'MULTI_OBJ')
-	lfield.observing = Table.read(obs_file,delimiter='|',
-		format='ascii.fixed_width', 
-		fill_values=[('--','0','DATE_OBS','TEXP')])
+    # Load observing details for multi-object follow-up
+    obs_file = xcasbahu.get_filename(field,'MULTI_OBJ')
+    lfield.observing = Table.read(obs_file,delimiter='|',
+        format='ascii.fixed_width',
+        fill_values=[('--','0','DATE_OBS','TEXP')])
 
-	# Load galaxies
-	sdss_file = xcasbahu.get_filename(field,'SDSS')
-	sdss_tab = Table.read(sdss_file)
-	# VSTACK
-	hectospec_file = xcasbahu.get_filename(field,'HECTOSPEC')
+    # Load galaxies
+    sdss_file = xcasbahu.get_filename(field,'SDSS')
+    sdss_tab = Table.read(sdss_file)
+    # VSTACK
+    hectospec_file = xcasbahu.get_filename(field,'HECTOSPEC')
 
-	lfield.galaxies = sdss_tab
+    lfield.galaxies = sdss_tab
 
-	# Return
-	return lfield
+    # Return
+    return lfield
